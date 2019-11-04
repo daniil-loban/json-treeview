5295cedac590c97027361a9e85c51c1d1a6759e7
daniil-loban <dungeon_lda@mail.ru>
2019-11-04 10:39:11 +0300

    refactor: rename variables, remove divs

--- src/components/App.js
+++ src/components/App.js
@@ -3,24 +3,28 @@ import { findDOMNode }  from "react-dom";
 import { data } from "../data";
 import Row from "./Row";
 
-data.byString = function(s) {
-  if (!!!s) return this;
-  let o = this;
-  const a = s.replace(/\[(\w+)\]/g, '.$1').replace(/^\./, '').split('.');
-  for (let i = 0, n = a.length; i < n; ++i) {
-    const k = a[i];
-    if (k in o) o = o[k];
+data.byString = function(pathInObject) {
+  if (!!!pathInObject) return this;
+  let objPart = this;
+  const pathPart = pathInObject.replace(/\[(\w+)\]/g, '.$1').replace(/^\./, '').split('.');
+  for (let i = 0, n = pathPart.length; i < n; ++i) {
+    const key = pathPart[i];
+    if (key in objPart) objPart = objPart[key];
     else return;
   }
-  return o;
+  return objPart;
 }
 
 const createTree = (treeData, level = 0, path='')=> {
   const children = treeData.children;
   const item = Object.keys(treeData)
-    .filter(field => field !== 'children')
-    .reduce((acc, e) => acc = {...acc, [e]: treeData[e]}, {})
-  return ([<Row {...item} level={level} path={path} data-path={path} />, 
+    .reduce((acc, e) => {
+      if (e !== 'children'){
+        acc = {...acc, [e]: treeData[e]}
+      }
+      return acc;
+    }, {})
+  return ([<Row {...item} level={level} path={path} data-path={path}/>, 
             children.map((e,i) => createTree(e, level + 1, `${path}.children[${i}]`))
           ]);
 };
@@ -36,7 +40,7 @@ class App extends Component {
     console.log(JSON.stringify(data, null, 2));
   }
   render(){
-    return <div onClick = {this.clickHandler}><div ref = {this.wrapper}>{createTree(data)}</div></div>  
+    return <section ref = {this.wrapper} onClick = {this.clickHandler}>{createTree(data)}</section>  
   }
 }
 
